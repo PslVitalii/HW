9f50ed10624bb49ebf6a7d59e93d11600af1e2d2
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-06-30 15:43:40 +0300

    Add ErrorHandler controller

---
+++ hw_3/src/main/java/com/epam/spring/homework3/controllers/ErrorHandlerController.java
@@ -0,0 +1,40 @@
+package com.epam.spring.homework3.controllers;
+
+import com.epam.spring.homework3.exceptions.EntityNotFoundException;
+import com.epam.spring.homework3.model.errors.ApiError;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestControllerAdvice
+public class ErrorHandlerController {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ApiError> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(BAD_REQUEST, error, ex));
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ApiError> handleMethodArgumentNotValid(BindException ex) {
+        ApiError apiError = new ApiError(BAD_REQUEST);
+        apiError.setMessage("Validation error");
+        apiError.setDebugMessage(ex.getMessage());
+        apiError.addValidationErrors(ex.getBindingResult().getFieldErrors());
+
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException e) {
+        return buildResponseEntity(new ApiError(NOT_FOUND, e.getMessage(), e));
+    }
+
+    private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
