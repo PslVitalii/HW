a2f4a4d5db074f2a7767b0d9ae1062a59104e991
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-07-01 21:19:56 +0300

    Implement authentication with JWT token

--- hw_3/src/main/java/com/epam/spring/homework3/controllers/ErrorHandlerController.java
+++ hw_3/src/main/java/com/epam/spring/homework3/controllers/ErrorHandlerController.java
@@ -3,8 +3,11 @@ package com.epam.spring.homework3.controllers;
 import com.epam.spring.homework3.exceptions.EntityAlreadyExistsException;
 import com.epam.spring.homework3.exceptions.EntityNotFoundException;
 import com.epam.spring.homework3.model.errors.ApiError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -38,6 +41,18 @@ public class ErrorHandlerController {
     @ExceptionHandler(EntityAlreadyExistsException.class)
     protected ResponseEntity<ApiError> handleEntityAlreadyExists(EntityAlreadyExistsException e) {
         return buildResponseEntity(new ApiError(BAD_REQUEST, e.getMessage(), e));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(BadCredentialsException e) {
+        String error = "Invalid username or password";
+        return buildResponseEntity(new ApiError(HttpStatus.FORBIDDEN, error, e));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException e) {
+        String error = "JWT token cannot be trusted";
+        return buildResponseEntity(new ApiError(HttpStatus.FORBIDDEN, error, e));
     }
 
     private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
