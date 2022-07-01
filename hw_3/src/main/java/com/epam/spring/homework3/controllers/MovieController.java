d65271ee4f769793dec7d4cdac8059db1d936861
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-07-01 14:20:24 +0300

    Add openapi for api documentation

--- hw_3/src/main/java/com/epam/spring/homework3/controllers/MovieController.java
+++ hw_3/src/main/java/com/epam/spring/homework3/controllers/MovieController.java
@@ -2,6 +2,10 @@ package com.epam.spring.homework3.controllers;
 
 import com.epam.spring.homework3.model.dto.MovieDto;
 import com.epam.spring.homework3.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,15 +13,14 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Movie API")
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
     private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
-
+    @Operation(description = "Save new movie")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public MovieDto saveMovie(@Valid @RequestBody MovieDto movie) {
@@ -25,23 +28,30 @@ public class MovieController {
         return movie;
     }
 
+    @Operation(description = "Delete movie by its id")
     @DeleteMapping("{id}")
     public void deleteMovie(@PathVariable long id){
         movieService.deleteMovie(id);
     }
 
+    @Operation(description = "Update movie")
     @PutMapping("{id}")
     public void updateMovie(@PathVariable long id, @Valid @RequestBody MovieDto movieDto){
         movieService.updateMovie(id, movieDto);
     }
 
+    @Operation(description = "Get movie by its id")
     @GetMapping("/{id}")
     public MovieDto getMovieById(@PathVariable long id){
         return movieService.getMovie(id);
     }
 
+    @Operation(description = "Get movies by name or get all movies")
     @GetMapping
-    public List<MovieDto> getMovies(@RequestParam(name = "name", required = false) String name){
+    public List<MovieDto> getMovies(
+            @Parameter(name = "name", description = "name of the movie or its part")
+            @RequestParam(name = "name", required = false) String name
+    ){
         if(name != null) {
             return movieService.getMoviesByName(name);
         }
