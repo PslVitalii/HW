dcd56ef6a587084ecdcd862042ceaea07d32dddd
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-17 21:01:16 +0300

    Clean AuthController

--- hw_3/src/main/java/com/epam/spring/homework3/controllers/AuthController.java
+++ hw_3/src/main/java/com/epam/spring/homework3/controllers/AuthController.java
@@ -10,21 +10,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 	private final UserService userService;
-	private final PasswordEncoder passwordEncoder;
 	private final AuthenticationManager authenticationManager;
 
 	@PostMapping("/signup")
@@ -45,8 +42,6 @@ public class AuthController {
 		String username = authentication.getName();
 		UserDto userDto = userService.getUser(username);
 
-		// return response entity containing user and authentication token
-		Map<String, Object> response = Map.of("user", userDto, "jwt", token);
 		return ResponseEntity.ok().header(HttpHeaders.AUTHORIZATION, token).body(userDto);
 	}
 }
