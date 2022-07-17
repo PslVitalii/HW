7904ddb7901bf058da3c10fe7df8dbc9752dca67
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-17 21:40:11 +0300

    Disable jackson annotations processing for Utils.objectToJson

--- hw_3/src/test/java/utils/Utils.java
+++ hw_3/src/test/java/utils/Utils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -8,6 +9,7 @@ public class Utils {
     public static String objectToJson(Object object) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(MapperFeature.USE_ANNOTATIONS);
 
         return objectMapper.writeValueAsString(object);
     }
