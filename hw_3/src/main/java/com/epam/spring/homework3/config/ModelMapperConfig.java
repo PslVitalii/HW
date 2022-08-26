a8ee0a51375c549909ed9295923c3025e0baa90a
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-08-26 17:52:30 +0300

    Add public access modifier

--- hw_3/src/main/java/com/epam/spring/homework3/config/ModelMapperConfig.java
+++ hw_3/src/main/java/com/epam/spring/homework3/config/ModelMapperConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 	@Bean
-	ModelMapper modelMapper() {
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 }
